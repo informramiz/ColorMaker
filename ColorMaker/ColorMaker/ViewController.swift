c09d5d19de37f9e92787a5a972f645319de0ffff
Ramiz Raja <informramiz@gmail.com>
2019-05-07 07:59:33 +0500

    feat: Connect actions and outlets

--- ColorMaker/ColorMaker/ViewController.swift
+++ ColorMaker/ColorMaker/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateColor()
+    }
 
-
+    @IBAction func updateColor() {
+        let r: CGFloat = self.redSwitch.isOn ? 1.0 : 0
+        let g: CGFloat = self.greenSwitch.isOn ? 1.0 : 0
+        let b: CGFloat = self.blueSwitch.isOn ? 1.0 : 0
+        self.colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
 }
 
